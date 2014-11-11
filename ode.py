a70f3b99c32f7ed99b8b2ae57f8b94b1512474ac
Chichi Lalescu <clalesc1@jhu.edu>
2014-11-11 14:10:37 -0500

    add base ode class, and first test

---
+++ ode.py
@@ -0,0 +1,91 @@
+#######################################################################
+#                                                                     #
+#  Copyright 2014 Cristian C Lalescu                                  #
+#                                                                     #
+#  This file is part of pyNT.                                         #
+#                                                                     #
+#  pyNT is free software: you can redistribute it and/or modify       #
+#  it under the terms of the GNU General Public License as published  #
+#  by the Free Software Foundation, either version 3 of the License,  #
+#  or (at your option) any later version.                             #
+#                                                                     #
+#  pyNT is distributed in the hope that it will be useful,            #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of     #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      #
+#  GNU General Public License for more details.                       #
+#                                                                     #
+#  You should have received a copy of the GNU General Public License  #
+#  along with pyNT.  If not, see <http://www.gnu.org/licenses/>       #
+#                                                                     #
+#######################################################################
+
+import numpy as np
+import sympy as sp
+import matplotlib.pyplot as plt
+from wiener import Wiener, get_t1ma_nm1
+
+class base_ODE(object):
+    def __init__(self):
+        return None
+    def Euler(self, h, nsteps, X0):
+        X = np.zeros((nsteps+1,) + X0.shape,
+                     X0.dtype)
+        X[0, :] = X0
+        for t in range(1, nsteps + 1):
+            X[t] = X[t-1] + self.rhs(X[t-1])*h
+        return X
+    def Taylor2(self, h, nsteps, X0):
+        X = np.zeros((nsteps+1,) + X0.shape,
+                     X0.dtype)
+        X[0, :] = X0
+        d = len(self.x)
+        llx = [sum(self.lx[j]*self.lx[i].diff(self.x[j])
+                   for j in range(d))
+               for i in range(d)]
+        acc = [sp.utilities.lambdify(tuple(self.x), llx[i], np)
+               for i in range(d)]
+        for t in range(1, nsteps+1):
+            velnum = self.rhs(X[t-1])
+            accnum = np.array([acc[i](*tuple(X[t-1]))
+                               for i in range(d)])
+            X[t] = (X[t-1]
+                    + velnum*    h
+                    + accnum*(.5*h**2))
+        return X
+    def get_evdt(
+            self,
+            h0 = 2.**(-3),
+            nsteps = 1,
+            X0 = None,
+            exp_range = range(8),
+            solver = ['Euler', 'Taylor2']):
+        x0 = [getattr(self, solver[0])(h0*2.**(-n), nsteps * 2**n, X0) for n in exp_range]
+        x1 = [getattr(self, solver[1])(h0*2.**(-n), nsteps * 2**n, X0) for n in exp_range]
+        dist = [(np.average(np.abs(x0[p][-1] - x1[p][-1]), axis = 0) /
+                 np.average(np.abs(x0[p][-1]), axis = 0))
+                for p in range(len(x0))]
+        err_vs_dt = np.zeros((len(x0), 4), dtype = X0.dtype)
+        err_vs_dt[:, 0] = np.array([h0*2.**(-n) for n in exp_range])
+        err_vs_dt[:, 2] = np.average(dist, axis = tuple(range(1, len(X0.shape))))
+        err_vs_dt[:, 1] = err_vs_dt[:, 2] - np.min(dist, axis = tuple(range(1, len(X0.shape))))
+        err_vs_dt[:, 3] = np.max(dist, axis = tuple(range(1, len(X0.shape)))) - err_vs_dt[:, 2]
+        return err_vs_dt
+    def get_system_dimension():
+        return None
+
+class ode(base_ODE):
+    def __init__(
+            self,
+            x = None,
+            f = None):
+        self.x = x
+        self.lx = [sp.sympify(fk) for fk in f]
+        self.rhs_func = [sp.utilities.lambdify(tuple(self.x), fk,  np)
+                           for fk in self.lx]
+        return None
+    def rhs(self, x):
+        return np.array([self.rhs_func[k](*tuple(x))
+                         for k in range(len(self.x))])
+    def get_system_dimension(self):
+        return len(self.x)
+
--- test.py
+++ test.py
@@ -21,47 +21,79 @@
 
 import numpy as np
 import sympy as sp
+import matplotlib.pyplot as plt
 
 from wiener import Wiener
 from sde import sde
+from ode import ode
 
-x = sp.Symbol('x')
+test_sde = False
+test_ode = True
 
-#bla = sde(
-#        x = [x],
-#        a = [x],
-#        b = [[x/2, sp.sin(x)/3]])
-#bla.get_evdt_vs_M(
-#        fig_name = 'figs/extra_tst',
-#        ntraj = 64,
-#        X0 = np.ones(1,).astype(np.float),
-#        h0 = .5,
-#        exp_range = range(8))
+if test_sde:
+    x = sp.Symbol('x')
+    bla = sde(
+            x = [x],
+            a = [x],
+            b = [[x/2, sp.sin(x)/3]])
+    bla.get_evdt_vs_M(
+            fig_name = 'figs/extra_tst',
+            ntraj = 64,
+            X0 = np.ones(1,).astype(np.float),
+            h0 = .5,
+            exp_range = range(8))
 
-#bla = sde(
-#        x = [x],
-#        a = [x],
-#        b = [[.5, .25]])
-#bla.get_evdt_vs_M(
-#        fig_name = 'figs/add_evdt',
-#        ntraj = 64,
-#        X0 = np.ones(1,).astype(np.float),
-#        h0 = .5,
-#        exp_range = range(8),
-#        solver = ['Taylor_2p0_additive', 'explicit_1p5_additive'])
+    bla = sde(
+            x = [x],
+            a = [x],
+            b = [[.5, .25]])
+    bla.get_evdt_vs_M(
+            fig_name = 'figs/add_evdt',
+            ntraj = 64,
+            X0 = np.ones(1,).astype(np.float),
+            h0 = .5,
+            exp_range = range(8),
+            solver = ['Taylor_2p0_additive', 'explicit_1p5_additive'])
 
-c = 0.0
-v = sp.Symbol('v')
-u = x**2 * (x**2 - 1 + c*x)
-bla = sde(
-        x = [x, v],
-        a = [v, -u.diff(x) - v],
-        b = [[.01, .1], [.95, .3]])
-bla.get_evdt_vs_M(
-        fig_name = 'figs/dwell_evdt',
-        ntraj = 32,
-        X0 = np.array([0., .0]).astype(np.float),
-        h0 = .5,
-        solver = ['Taylor_2p0_additive', 'explicit_1p5_additive'],
-        exp_range = range(8))
+    c = 0.0
+    v = sp.Symbol('v')
+    u = x**2 * (x**2 - 1 + c*x)
+    bla = sde(
+            x = [x, v],
+            a = [v, -u.diff(x) - v],
+            b = [[.01, .1], [.95, .3]])
+    bla.get_evdt_vs_M(
+            fig_name = 'figs/dwell_evdt',
+            ntraj = 32,
+            X0 = np.array([0., .0]).astype(np.float),
+            h0 = .5,
+            solver = ['Taylor_2p0_additive', 'explicit_1p5_additive'],
+            exp_range = range(8))
+
+if test_ode:
+    x = sp.Symbol('x')
+    y = sp.Symbol('y')
+    z = sp.Symbol('z')
+    rho = 28.
+    sigma = 10.
+    beta = 8./3
+    lorenz_rhs = [sigma*(y - x),
+                  x*(rho - z) - y,
+                  x*y - beta*z]
+    bla = ode(
+            x = [x, y, z],
+            f = lorenz_rhs)
+    evdt = bla.get_evdt(X0 = 10*np.random.random((3, 128)))
+    fig = plt.figure(figsize = (6,6))
+    ax = fig.add_subplot(111)
+    ax.errorbar(
+            evdt[:, 0],
+            evdt[:, 2],
+            yerr = [evdt[:, 1], evdt[:, 3]],
+            label = 'Euler vs Taylor 2')
+    ax.plot(evdt[:, 0], evdt[:, 0], label = '$\\Delta t$')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.legend(loc = 'best')
+    fig.savefig('figs/ode_evdt.pdf', format = 'pdf')
 
